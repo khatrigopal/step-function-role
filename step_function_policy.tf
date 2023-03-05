f6d7bd7e89b13d4e661f91de8e60b27c1f0deaea
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-05 19:31:17 +0530

    Update step_function_policy.tf

--- step_function_policy.tf
+++ step_function_policy.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "stepfunction_exectuion_role" {
  name  = "step-function-execution-role"
- assume_role_policy = data_step_function.aws_iam_policy_document.stepfunction_execution_role.json
+ assume_role_policy = data_step_funtion.aws_iam_policy_document.stepfunction_execution_role.json
 }
 
 resource "aws_iam_policy" "stepfunction_exectuion_policy" {
  name  = "step-function-execution-policy"
- policy = data_step_function.aws_iam_policy_document.stepfunction_execution_policy.json
+ policy = data_step_funtion.aws_iam_policy_document.stepfunction_execution_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "stepfunction_custom_attachment" {
