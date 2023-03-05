c486a5b10f3c38c3e4143517869edf6642d96e50
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-05 19:40:15 +0530

    Update step_function_policy.tf

--- step_function_policy.tf
+++ step_function_policy.tf
@@ -10,5 +10,5 @@ resource "aws_iam_policy" "stepfunction_exectuion_policy" {
 
 resource "aws_iam_role_policy_attachment" "stepfunction_custom_attachment" {
  policy_arn = aws_iam_policy.stepfunction_exectuion_policy.arn
- role = aws_iam_role.stepfunction_exectuion_role
+ role = aws_iam_role.stepfunction_exectuion_role.name
 }
