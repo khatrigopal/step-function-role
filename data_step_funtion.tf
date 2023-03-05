700aaa0478209ffc583b9942d6146cb8e20bd864
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-05 19:07:11 +0530

    Update data_step_funtion.tf

--- data_step_funtion.tf
+++ data_step_funtion.tf
@@ -1,6 +1,7 @@
 data "aws_iam_policy_document" "stepfunction_execution_role" {
  statement {
    actions = ["sts.AssumeRole"]
+   effect = "Allow"
    principals {
       identification = ["states.amazonaws.com"]
       type           = Service
@@ -8,4 +9,23 @@ data "aws_iam_policy_document" "stepfunction_execution_role" {
  }
 }
 
-
+data "aws_iam_policy_document" "stepfunction_execution_policy" {
+ stetement {
+  effect = "Allow"
+  actions = [
+          "states:List*",
+          "states:Describe*",
+          "states:Get*",
+          "states:StartExecution",
+          "states:StopExecution"
+        ]
+  resource = "*"
+ }
+  
+ stetement {
+  effect = "Allow"
+  actions = ["lambda:InvokeFunction"]
+  resource = "*" 
+ }
+ }
+   
